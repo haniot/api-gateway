7c6982158d6a4e29f722062fc20ddc87c7190888
AdalcinoJunior-PCNUTES <adaljunior.inf@gmail.com>
2018-11-28 16:31:47 -0300

    Adding swagger documentation

--- eg-plugin-haniot/manifest.js
+++ eg-plugin-haniot/manifest.js
@@ -6,8 +6,8 @@ module.exports = {
     pluginContext.registerPolicy(require('./policies/auth/haniot-auth-policy'));
     pluginContext.registerPolicy(require('./policies/body-parser/haniot-body-parser-policy'));
     pluginContext.registerCondition(require('./conditions/is-auth'));
-    // pluginContext.registerGatewayRoute(require('./routes/user.router-gateway'));
-
+    pluginContext.registerGatewayRoute(require('./routes/documentation-haniot'));
+    pluginContext.registerAdminRoute(require('./routes/documentation-apigw-admin'));
   },
   policies:['haniot-jwt-policy', 'haniot-jwtScopes-policy', 'haniot-auth-policy','haniot-body-parser-policy']
 };
