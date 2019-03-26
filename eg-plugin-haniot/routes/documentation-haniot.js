603a4ac4c31c6d7e85b46bb96ba0b4ac0af165f1
root <adaljunior.inf@gmail.com>
2019-03-26 19:00:30 -0300

    [FT05] - Cadastro de Pacientes User Stories: Como profissional de saúde eu quero realizar o cadastro de um paciente por meio de um questionário. Bugs: Nenhum. Outras Informações: Configurando documentação swagger.

--- eg-plugin-haniot/routes/documentation-haniot.js
+++ eg-plugin-haniot/routes/documentation-haniot.js
@@ -2,12 +2,36 @@
  * Routes to expose HANIoT API swagger documentation
  */
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
 
-module.exports = function (expressGatewayApp) {    
-  const documentSwagger = YAML.load('./haniot-haniot-apigw-v1-swagger.yaml');
+module.exports = function (expressGatewayApp) {
   const options = {
-    customCss: '.swagger-ui .topbar { display: none }',
+    swaggerUrl: 'https://api.swaggerhub.com/apis/haniot/haniot-apigw/v1/swagger.json',
+    customCss: `.swagger-ui .topbar { 
+                    background-color: #00a594;  
+                    padding: 10px 0; 
+                }
+                .swagger-ui .topbar .topbar-wrapper a span { 
+                  display: none; 
+                }
+                .swagger-ui .topbar .topbar-wrapper a { 
+                    display: none;
+                }
+                .swagger-ui .topbar .topbar-wrapper:before { 
+                  content: url(http://nutes.uepb.edu.br/wp-content/uploads/2016/05/footer-N.fw_.png);
+                  width: 20px;
+                  height: 20px;
+                  transform: scale(0.05);
+                  margin-left: -5px;
+                  margin-top: -50px;
+              }
+                .swagger-ui .topbar .topbar-wrapper:after {  
+                    content: 'HANIoT';
+                    margin: auto 25px; 
+                    padding: 10px 10px;
+                    font-size: 1.7em;
+                    font-weight: 700;
+                    color: #fff; 
+                }`,
     customfavIcon: 'http://nutes.uepb.edu.br/wp-content/uploads/2014/01/icon.fw_.png',
     customSiteTitle: `API Reference | HANIoT`
   }
@@ -16,6 +40,6 @@ module.exports = function (expressGatewayApp) {
   });
 
   expressGatewayApp.use('/api/v1/reference', swaggerUi.serve, (req, res) => {
-    swaggerUi.setup(documentSwagger, options)(req, res);
+    swaggerUi.setup(null, options)(req, res);
   });
 };
